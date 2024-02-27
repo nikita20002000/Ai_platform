de24aea40c7957244e105babf67d06698d6b3f7b
nikita20002000 <vertihost@yandex.com>
2024-02-27 18:15:22 +0300

    Изменения таск менеджера

--- web_app/to_do_list/static/to_do_list/js/to_do_list/task_word_end.js
+++ web_app/to_do_list/static/to_do_list/js/to_do_list/task_word_end.js
@@ -12,12 +12,18 @@ function getNoun(number) {
     number = Math.abs(number) % 100;
     var num = number % 10;
 
-    if (number > 10 && number < 20)
+    if (number > 10 && number < 20) {
         return 2;
-    if (num > 1 && num < 5)
+    }
+    if (num >= 5 && num < 10){
+        return 2;
+    }
+    if (num > 1 && num < 5) {
         return 1;
-    if (num == 1)
+    }
+    if (num == 1) {
         return 0;
+    }
 }
 
 
