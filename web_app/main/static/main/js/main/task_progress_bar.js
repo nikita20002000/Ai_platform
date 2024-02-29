3d3678472bb5b346d4a90684be5e1419b2040c62
nikita20002000 <vertihost@yandex.com>
2024-02-29 16:19:36 +0300

    Изменения

--- web_app/main/static/main/js/main/task_progress_bar.js
+++ web_app/main/static/main/js/main/task_progress_bar.js
@@ -6,7 +6,7 @@ var task_list = bar.textContent.split(' ')
 var remain = task_list[0]
 var all = task_list[1]
 
-var percent = (remain / all) * 100
+var percent = Math.round(100 - (remain / all) * 100, 1)
 
 bar.style.cssText += `width: ${percent}%;`;
 value.textContent = `${percent}%`
