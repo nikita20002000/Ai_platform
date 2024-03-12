2b5fed7a1bf6eaacc18f8269987d22f419944db0
nikita20002000 <vertihost@yandex.com>
2024-03-12 19:18:10 +0300

    Добавил импорт в csv файл

--- web_app/sells/static/sells/js/csv_converter.js
+++ web_app/sells/static/sells/js/csv_converter.js
@@ -1,3 +1,52 @@
-var a = document.querySelectorAll('td')
+class csvExport {
+    constructor(table, header = true) {
+        this.table = table;
+        this.rows = Array.from(table.querySelectorAll("tr"))
+    }
+    exportCsv() {
+        const lines = [];
+        const ncols = this.long_row();
 
-console.log(a.values())
+        for (const row of this.rows) {
+            let line = '';
+            for (let i=0; i < ncols; i++) {
+
+                if (row.children[i] !==undefined) {
+                    line += row.children[i].textContent;
+                }
+                line += i !== ncols - 1 ? ";" : "";
+            }
+            lines.push(line)
+
+        }
+        for (let j=1; j < ncols; j++) {
+            lines[j] = String(lines[j]).replace(/\n/gi, '');
+
+        }
+        return lines.join('\n');
+
+    }
+
+    long_row() {
+        return this.rows.reduce((length, row) => (row.childElementCount > length ? row.childElementCount : length), 0)
+    }
+}
+
+const btnExport = document.querySelector('#btnExport')
+const tableElement = document.querySelector('table')
+
+btnExport.addEventListener("click", () => {
+    const obj = new csvExport(tableElement)
+    const csv_data = (obj.exportCsv());
+    const blob = new Blob([csv_data], {type: "text/csv" });
+
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download='file.csv';
+    a.click();
+
+    setTimeout(() => {
+        URL.revokeObjectURL(url);
+    }, 500);
+    });
